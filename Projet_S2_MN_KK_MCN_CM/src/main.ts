2c120ee8bf91beb1d4affdd8747b265ec7b715e9
Nezzar-Marwa <marwa.nezzar@edu.univ-fcomte.fr>
2023-05-16 10:43:49 +0200

    point d'entré application

--- Projet_S2_MN_KK_MCN_CM/src/main.ts
+++ Projet_S2_MN_KK_MCN_CM/src/main.ts
@@ -3,10 +3,16 @@ import './assets/css/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import routes from '~pages'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(
+    createRouter({
+      history: createWebHistory(),
+      routes
+    })
+  )
 
 app.mount('#app')
