df6c90ed42d22533b3f537c2677af80383c54c0e
Nezzar-Marwa <marwa.nezzar@edu.univ-fcomte.fr>
2023-05-17 09:43:50 +0200

    color import

--- Projet_S2_MN_KK_MCN_CM/tailwind.config.js
+++ Projet_S2_MN_KK_MCN_CM/tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    
+    colors: {
+      toniccolor: '#ff9c40',
+      backgroundcolor: '#f5f5f5',
+      contrastcolor: '#2c2b2b',
+      contentbluelayout: '#2f8a99',
+      green: '#2c2c2c',
+      text: '#000000',
+      contentbluecards: '#8ecdd7',
+      teamcolor: '#434343'
+     },
     extend: {},
   },
   plugins: [],
