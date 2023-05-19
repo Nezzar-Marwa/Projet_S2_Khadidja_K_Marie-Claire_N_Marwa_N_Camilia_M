b8d23f4996bb87066b2d53d9ab7347cbd0e0b5a1
Nezzar-Marwa <marwa.nezzar@edu.univ-fcomte.fr>
2023-05-19 15:27:18 +0200

    footer

--- Projet_S2_MN_KK_MCN_CM/src/components/FooterPage.vue
+++ Projet_S2_MN_KK_MCN_CM/src/components/FooterPage.vue
@@ -1,3 +1,47 @@
+<script setup lang="ts">
+import 
+</script>
+
 <template>
-    Template pour le pied de page
-  </template>
+  <footer>
+
+    <div>
+      <h4>Nous</h4>
+      <address>
+        <a href="tel:0384542727">03 84 54 27 27</a><br />
+        <a href="mailto:Foksers@contact.fr">foksers@contact.fr</a>
+      </address>
+    </div>
+
+    <nav>
+      <h4>Liens utiles</h4>
+      <ul>
+        <li><a href="#">Mentions légales</a></li>
+        <li><a href="#">Donations</a></li>
+        <li><a href="#">Newsletter</a></li>
+      </ul>
+    </nav>
+
+
+
+    <div></div>
+
+    <div>
+      <h4>Nous suivre</h4>
+      <ul>
+        <li>
+          <a href="https://facebook.com" aria-label="facebook"> </a>
+        </li>
+        <li>
+          <a href="https://instagram.com" aria-label="instagram"> </a>
+        </li>
+        <li>
+          <a href="https://twitter.com" aria-label="twitter"> </a>
+        </li>
+        <li>
+          <a href="https://youtube.com" aria-label="youtube"> </a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
