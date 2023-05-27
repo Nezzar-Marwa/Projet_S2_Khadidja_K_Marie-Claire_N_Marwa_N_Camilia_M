fbdafd4b58f75828ab7581831da3913cc163cc0c
Nezzar-Marwa <marwa.nezzar@edu.univ-fcomte.fr>
2023-05-27 21:10:57 +0200

    footer desktop

--- Projet_S2_MN_KK_MCN_CM/src/components/FooterPage.vue
+++ Projet_S2_MN_KK_MCN_CM/src/components/FooterPage.vue
@@ -6,9 +6,9 @@ import InstagramIcon from '@/components/icons/IconInstagram.vue'
 </script>
 
 <template>
-  <footer class="space-y-6 bg-contrastcolor py-16 px-6 text-backgroundcolor">
+  <footer class="grille space-y-6 bg-contrastcolor py-16 px-6 text-backgroundcolor">
 
-    <div>
+    <div class="col-span-2 col-start-1 lg:mt-7 ">
       <h4 class="font-poppins text-base font-semibold uppercase mb-2">Nous</h4>
       <address class="font-poppins text-sm">
         <a href="tel:0384542727">03 84 54 27 27</a><br />
@@ -16,7 +16,7 @@ import InstagramIcon from '@/components/icons/IconInstagram.vue'
       </address>
     </div>
 
-    <nav>
+    <nav class="col-span-2 col-start-4 ">
       <h4 class="font-poppins text-base font-semibold uppercase mb-2">Liens utiles</h4>
       <ul class="font-poppins text-sm">
         <li><a href="#">Mentions légales</a></li>
@@ -29,7 +29,7 @@ import InstagramIcon from '@/components/icons/IconInstagram.vue'
 
     <div></div>
 
-    <div>
+    <div class="col-span-2 col-start-7">
       <h4 class="font-poppins text-base font-semibold uppercase mb-3 ">Nous suivre</h4>
       <ul class="flex gap-4">
         <li>
@@ -43,29 +43,29 @@ import InstagramIcon from '@/components/icons/IconInstagram.vue'
         </li>
       </ul>
     </div>
-    <section>
+    <section class="col-span-3 col-start-9 space-y-3">
       <h4 class="mb-8 font-poppins text-center text-sm">Une remarque ? une suggestion ? un commentaire ? N’hésitez pas à nous en faire part.
           Car pour nous votre avis compte</h4>
       <form action="#">
       <div>
         <label for="Prénom*">Prénom<span class="text-toniccolor">*</span> </label>
-        <input class="flex rounded-lg text-text border mb-4 px-12 py-1  border-toniccolor focus:outline-none focus:ring-1 focus:ring-toniccolor " type="text" name="Prénom" id="Prénom">
+        <input class="flex rounded-lg text-text border mb-4 px-12 py-1  border-toniccolor focus:outline-none focus:ring-1 focus:ring-toniccolor lg:px-24 " type="text" name="Prénom" id="Prénom">
       </div>
       <div>
         <label for="Nom*">Nom<span class="text-toniccolor">*</span></label>
-        <input class="flex rounded-lg text-text border mb-4 px-12 py-1  border-toniccolor focus:outline-none focus:ring-1 focus:ring-toniccolor " type="text" name="Nom" id="Nom">
+        <input class="flex rounded-lg text-text border mb-4 px-12 py-1  border-toniccolor focus:outline-none focus:ring-1 focus:ring-toniccolor lg:px-24  " type="text" name="Nom" id="Nom">
       </div>
       <div>
         <label for="Mail*">Mail<span class="text-toniccolor">*</span></label>
-        <input class="flex rounded-lg text-text border mb-4 px-12 py-1  border-toniccolor  invalid:border-invalid invalid:text-invalid focus:outline-none focus:ring-1 focus:ring-toniccolor focus:invalid:border-invalid focus:invalid:ring-invalid" type="email" placeholder="VotreEmail@gmail.com" name="Mail">
+        <input class="flex rounded-lg text-text border mb-4 px-12 py-1  border-toniccolor  invalid:border-invalid invalid:text-invalid focus:outline-none focus:ring-1 focus:ring-toniccolor focus:invalid:border-invalid focus:invalid:ring-invalid lg:px-24 " type="email" placeholder="VotreEmail@gmail.com" name="Mail">
       </div>
       <div>
         <label for="Téléphone*">Téléphone<span class="text-toniccolor">*</span></label>
-        <input class="flex rounded-lg text-text border mb-4 px-12 py-1  border-toniccolor  invalid:border-invalid invalid:text-invalid focus:outline-none focus:ring-1 focus:ring-toniccolor focus:invalid:border-invalid focus:invalid:ring-invalid" type="text" placeholder="Votre numéro ici" name="Téléphone">
+        <input class="flex rounded-lg text-text border mb-4 px-12 py-1  border-toniccolor  invalid:border-invalid invalid:text-invalid focus:outline-none focus:ring-1 focus:ring-toniccolor focus:invalid:border-invalid focus:invalid:ring-invalid lg:px-24 " type="text" placeholder="Votre numéro ici" name="Téléphone">
       </div>
       <div>
         <label for="message*">Message<span class="text-toniccolor">*</span></label>
-        <textarea class="flex rounded-lg text-text border mb-4 px-16 py-1  border-toniccolor focus:outline-none focus:ring-1 focus:ring-toniccolor "  id="msg" name="message" rows="5"></textarea>
+        <textarea class="flex rounded-lg text-text border mb-4 px-16 py-1  border-toniccolor focus:outline-none focus:ring-1 focus:ring-toniccolor lg:px-28"  id="msg" name="message" rows="5"></textarea>
       </div>
       <div>
       <button class="rounded-full border border-toniccolor px-10 py-3 font-bold text-backgroundcolor hover:bg-toniccolor"  type="submit">Envoyer</button>
