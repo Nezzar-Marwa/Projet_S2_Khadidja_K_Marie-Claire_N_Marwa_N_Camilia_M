8d8c31f3138e31542eba4accd14b70446badbb82
Nezzar-Marwa <marwa.nezzar@edu.univ-fcomte.fr>
2023-05-27 20:11:19 +0200

    formulaire de contact

--- Projet_S2_MN_KK_MCN_CM/src/components/FooterPage.vue
+++ Projet_S2_MN_KK_MCN_CM/src/components/FooterPage.vue
@@ -44,26 +44,28 @@ import InstagramIcon from '@/components/icons/IconInstagram.vue'
       </ul>
     </div>
     <section>
+      <h4 class="mb-8 font-poppins text-center">Une remarque ? une suggestion ? un commentaire ? N’hésitez pas à nous en faire part.
+          Car pour nous votre avis compte</h4>
       <form action="#">
       <div>
         <label for="Prénom*">Prénom<span class="text-toniccolor">*</span> </label>
-        <input class="flex rounded-lg text-text border mb-4 px-5  border-toniccolor focus:outline-none focus:ring-1 focus:ring-toniccolor " type="text" name="Prénom" id="Prénom">
+        <input class="flex rounded-lg text-text border mb-4 px-12 py-1  border-toniccolor focus:outline-none focus:ring-1 focus:ring-toniccolor " type="text" name="Prénom" id="Prénom">
       </div>
       <div>
         <label for="Nom*">Nom<span class="text-toniccolor">*</span></label>
-        <input class="flex rounded-lg text-text border mb-4 px-5  border-toniccolor focus:outline-none focus:ring-1 focus:ring-toniccolor " type="text" name="Nom" id="Nom">
+        <input class="flex rounded-lg text-text border mb-4 px-12 py-1  border-toniccolor focus:outline-none focus:ring-1 focus:ring-toniccolor " type="text" name="Nom" id="Nom">
       </div>
       <div>
         <label for="Mail*">Mail<span class="text-toniccolor">*</span></label>
-        <input class="flex rounded-lg text-text border mb-4 px-5  border-toniccolor  invalid:border-invalid invalid:text-invalid focus:outline-none focus:ring-1 focus:ring-toniccolor focus:invalid:border-invalid focus:invalid:ring-invalid" type="email" placeholder="VotreEmail@gmail.com" name="Mail">
+        <input class="flex rounded-lg text-text border mb-4 px-12 py-1  border-toniccolor  invalid:border-invalid invalid:text-invalid focus:outline-none focus:ring-1 focus:ring-toniccolor focus:invalid:border-invalid focus:invalid:ring-invalid" type="email" placeholder="VotreEmail@gmail.com" name="Mail">
       </div>
       <div>
         <label for="Téléphone*">Téléphone<span class="text-toniccolor">*</span></label>
-        <input class="flex rounded-lg border mb-4 px-5 border-toniccolor" type="text" name="Téléphone">
+        <input class="flex rounded-lg text-text border mb-4 px-12 py-1  border-toniccolor  invalid:border-invalid invalid:text-invalid focus:outline-none focus:ring-1 focus:ring-toniccolor focus:invalid:border-invalid focus:invalid:ring-invalid" type="text" placeholder="Votre numéro ici" name="Téléphone">
       </div>
       <div>
         <label for="message*">Message<span class="text-toniccolor">*</span></label>
-        <textarea class="flex rounded-lg text-text border mb-4 px-8  border-toniccolor focus:outline-none focus:ring-1 focus:ring-toniccolor "  id="msg" name="message" rows="5"></textarea>
+        <textarea class="flex rounded-lg text-text border mb-4 px-16 py-1  border-toniccolor focus:outline-none focus:ring-1 focus:ring-toniccolor "  id="msg" name="message" rows="5"></textarea>
       </div>
       <div>
       <button class="rounded-full border border-toniccolor px-10 py-3 font-bold text-backgroundcolor hover:bg-toniccolor"  type="submit">Envoyer</button>
