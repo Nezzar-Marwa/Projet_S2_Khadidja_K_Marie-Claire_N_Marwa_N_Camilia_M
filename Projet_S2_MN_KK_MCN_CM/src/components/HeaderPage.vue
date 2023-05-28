dc7a5ff40538d1351c878d241df875b4d4b4f237
Nezzar-Marwa <marwa.nezzar@edu.univ-fcomte.fr>
2023-05-28 17:46:28 +0200

    header desktop

--- Projet_S2_MN_KK_MCN_CM/src/components/HeaderPage.vue
+++ Projet_S2_MN_KK_MCN_CM/src/components/HeaderPage.vue
@@ -13,31 +13,33 @@
 </script>
 
 <template>
-  <header class="fixed z-10 flex w-full translate-y-0 items-stretch justify-between bg-backgroundcolor px-6 py-2 transition-all duration-300 ease-in-out">
+  <header class="fixed z-10 flex w-full translate-y-0 items-stretch justify-between bg-backgroundcolor px-6 py-2 transition-all duration-300 ease-in-out lg:py-0">
     <div class="flex items-center">
       <a href="/">
         <LogoIcon />
       </a>
     </div>
 
-    <div class="flex items-center gap-4">
+    <div class="flex items-center gap-4 lg:flex-row-reverse">
 
-      <button class="relative z-10 flex h-5 w-8 flex-col justify-between" @click="activeMenu = !activeMenu"
+      <button class="relative z-10 flex h-5 w-8 flex-col justify-between lg:hidden" @click="activeMenu = !activeMenu"
       > 
-        <div class="ease h-[2px] w-full transform rounded-full bg-text transition duration-300" :class="{ 'translate-y-[9px] rotate-45 bg-white': activeMenu }"></div>
-        <div class="ease h-[2px] w-full transform rounded-full bg-text transition duration-300" :class="{ 'bg-white opacity-0': activeMenu }"></div>
-        <div class="ease h-[2px] w-full transform rounded-full bg-text transition duration-300" :class="{ '-translate-y-[9px] -rotate-45 bg-white': activeMenu }"></div>
+        <div class="ease h-[2px] w-full transform rounded-full bg-text transition duration-300" :class="{ 'translate-y-[9px] rotate-45 bg-backgroundcolor': activeMenu }"></div>
+        <div class="ease h-[2px] w-full transform rounded-full bg-text transition duration-300" :class="{ 'bg-backgroundcolor opacity-0': activeMenu }"></div>
+        <div class="ease h-[2px] w-full transform rounded-full bg-text transition duration-300" :class="{ '-translate-y-[9px] -rotate-45 bg-backgroundcolor': activeMenu }"></div>
       </button>
 
-			<nav class="invisible opacity-0 fixed inset-0 h-screen w-screen bg-contrastcolor text-2xl text-backgroundcolor transition-all duration-300 ease-in-out"
-      :class="{ '!visible opacity-100': activeMenu }">
-		    <ul class="mt-[25vh] ml-16">
-		      <li><RouterLink class="menu-link" to="/ressources" @click="closeMenu">Ressources</RouterLink></li>
-		      <li><RouterLink class="menu-link" to="/communaute" @click="closeMenu">Communauté</RouterLink></li>
-		      <li><RouterLink class="menu-link" to="/cartes" @click="closeMenu">Cartes interactives</RouterLink></li>
-          <li><RouterLink class="menu-link" to="/a_propos" @click="closeMenu">A propos</RouterLink></li>
-		      <li><RouterLink class="menu-link" to="/contact" @click="closeMenu">Contact</RouterLink></li>
-		      <li><RouterLink class="menu-link" to="/" @click="closeMenu">S'inscrire</RouterLink></li>
+			<nav class="invisible opacity-0 fixed inset-0 h-screen w-screen bg-contrastcolor text-2xl text-backgroundcolor transition-all duration-300 ease-in-out lg:visible lg:relative lg:pt-4 lg:flex lg:h-auto lg:w-auto lg:items-center lg:bg-transparent lg:text-sm lg:font-semibold lg:uppercase lg:tracking-wide lg:text-text lg:bg-backgroundcolor lg:opacity-100"
+      :class="{ '!visible opacity-100': activeMenu }"
+      v-scroll-lock="activeMenu"
+      >
+		    <ul class="mt-[25vh] ml-16 lg:m-0 lg:flex">
+		      <li class="menu-item"><RouterLink class="menu-link" to="/ressources" @click="closeMenu">RESSOURCES</RouterLink></li>
+		      <li class="menu-item"><RouterLink class="menu-link" to="/communaute" @click="closeMenu">COMMUNAUTE</RouterLink></li>
+		      <li class="menu-item"><RouterLink class="menu-link" to="/cartes" @click="closeMenu">CARTES INTERACTIVES</RouterLink></li>
+          <li class="menu-item"><RouterLink class="menu-link" to="/a_propos" @click="closeMenu">A PROPOS</RouterLink></li>
+		      <li class="menu-item"><RouterLink class="menu-link mb-9" to="/contact" @click="closeMenu">CONTACT</RouterLink></li>
+		      <li class="menu-item"><RouterLink class="menu-link lg:mt-none" to="/" @click="closeMenu"><button class="rounded-full border border-toniccolor px-10 py-3 font-poppins font-medium text-backgroundcolor bg-toniccolor">S'INSCRIRE</button></RouterLink></li>
 		    </ul>
 		  </nav>
     </div>
